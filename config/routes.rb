984b1e1d23b647ed00d6062f8ec67ac5496ddbf2
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-02 18:26:26 -0700

    styled items index with nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root "welcome#index"
+
+  get "/login", to: "sessions#new"
+  post '/login', to: "sessions#create"
+  get '/logout', to: "sessions#destroy"
+  get '/cart', to: "cart#show"
   resources :items, only: [:index]
 
   get "/:category_name", to: "categories#show"
