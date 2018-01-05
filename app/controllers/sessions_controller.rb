a62f87299f6f02d85251ab2418a7ff837002d672
leepuppychow <leepuppychow@gmail.com>
2018-01-04 17:57:07 -0700

    Flash message for user creation and login fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
+      flash[:notice] = "Logged in as #{@user.username}"
       redirect_to dashboard_path
     else
       render :new
