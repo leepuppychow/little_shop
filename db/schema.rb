a3448a72ab7611ed1b6c8f65f0025314c2f12986
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-07 02:20:10 -0700

    added quantity and subtotal to OrderItems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180106211359) do
+ActiveRecord::Schema.define(version: 20180107085856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20180106211359) do
     t.bigint "order_id"
     t.bigint "item_id"
     t.float "original_price"
+    t.integer "quantity"
+    t.float "sub_total"
     t.index ["item_id"], name: "index_order_items_on_item_id"
     t.index ["order_id"], name: "index_order_items_on_order_id"
   end
