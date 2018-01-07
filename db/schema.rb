63018cf601591320de3214e9e619d23300c9c978
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-07 02:48:31 -0700

    Added a default value of 0 to users table for authentication purposes

--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 20180107085856) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.integer "role"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "items", "categories"
