0ccac0db7863d962310c19c1c40c94489f525330
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-07 10:45:51 -0700

    authenticated user cannot view another persons private data

---
+++ spec/features/users/authenticated_user_cannot_view_another_persons_private_data_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "authenticated user cannot view another persons data" do
+  before :each do
+    @user1 = create(:user)
+    @user2 = create(:user, username: "Gabe")
+    @order1 = create(:order, user: @user1)
+    @order2 = create(:order, status: "pending", user: @user2)
+  end
+
+  context "when a registered user visits a page not their own" do
+    it "should render a 404 page not found message" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+
+      visit "users/#{@user1.id}"
+      expect(page).to have_content("Dashboard for #{@user1.username}")
+
+      visit dashboard_path
+      # save_and_open_page
+      byebug
+      expect(page).to have_content("The page you were looking for doesn't exist")
+    end
+  end
+end
--- spec/features/users/registered_user_can_see_past_orders_spec.rb
+++ spec/features/users/registered_user_can_see_past_orders_spec.rb
@@ -1,31 +1,35 @@
 require 'rails_helper'
 
 describe "When a registered user visits the orders path" do
+  before :each do
+    @user1 = create(:user)
+    @user2 = create(:user)
+    @order1 = create(:order, user: @user1)
+    @order2 = create(:order, status: "pending", user: @user1)
+  end
+
   it "sees all of the orders belonging to him/her in a table" do
-    user = create(:user)
-    order1 = create(:order, user: user)
-    order2 = create(:order, status: "pending", user: user)
     item1, item2, item3 = create_list(:item, 3)
     item4 = create(:item)
-    order1.items << [item1,item2,item3]
-    order2.items << [item3,item4]
+    @order1.items << [item1,item2,item3]
+    @order2.items << [item3,item4]
 
     visit root_path
     click_on "Login"
-    fill_in "username", with: user.username
-    fill_in "password", with: user.password
+    fill_in "username", with: @user1.username
+    fill_in "password", with: @user1.password
     click_on "Submit"
 
     expect(current_path).to eq dashboard_path
-
     click_on "My Orders"
 
     expect(current_path).to eq orders_path
-    expect(page).to have_content order1.status
-    expect(page).to have_content order2.status
-    expect(page).to have_content order1.total_price
-    expect(page).to have_content order2.total_price
-    expect(page).to have_content order1.address
-    expect(page).to have_content order2.address
+    expect(page).to have_content @order1.status
+    expect(page).to have_content @order2.status
+    expect(page).to have_content @order1.total_price
+    expect(page).to have_content @order2.total_price
+    expect(page).to have_content @order1.address
+    expect(page).to have_content @order2.address
   end
+
 end
