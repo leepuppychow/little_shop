07820ee0c29816567c49d936962b15b05e8311f1
leepuppychow <leepuppychow@gmail.com>
2018-01-07 18:50:39 -0700

    Highest priced item for category complete

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,10 @@ class Category < ApplicationRecord
   before_save :generate_slug
   validates :name, presence: true
   has_many :items
-  #
-  # def highest_priced_item_per_category
-  #   Category.
-  # end
+
+  def self.highest_priced_item_per_category
+    joins(:items).group(:name).maximum(:price) 
+  end
 
 
   private
