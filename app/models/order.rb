0314f1f6ca5ba715769ebb9e4be5e26536497d4a
leepuppychow <leepuppychow@gmail.com>
2018-01-08 14:55:06 -0700

    adding scope to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,8 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :items, through: :order_items
   validates_presence_of :status, :total_price, :created_at, :updated_at
+  scope :ordered_by_id, -> {order(:id)}
+  
 
   def item_count
     items.group(:name).count
