4496741e83d042c777aa65d00526f31a389e629e
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-07 18:11:44 -0700

    WIP analytics dashboard should show highest priced item per category

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -2,6 +2,7 @@ class Admin::AnalyticsController < Admin::BaseController
 
   def index
     @user = current_user
+    @categories = Category.all
   end
 
 end
--- spec/features/admin/admin_can_visit_the_analytics_dash_spec.rb
+++ spec/features/admin/admin_can_visit_the_analytics_dash_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 describe "when an admin visits the admin dashboard" do
   before :each do
     @admin = create(:user, role: 1)
+    @category1 = create(:category)
+    @category2 = create(:category, name: "Jedi")
+    @item1 = create(:item, category: @category1, price: 600.0)
+    @item2 = create(:item, category: @category1)
+    @item3 = create(:item, category: @category2, price: 700.0)
+    @item4 = create(:item, category: @category2)
+    @item5 = create(:item, category: @category2)
     @order1 = create(:order, user: @admin)
     @order2 = create(:order, user: @admin)
     @order3 = create(:order, user: @admin)
@@ -24,5 +31,12 @@ describe "when an admin visits the admin dashboard" do
   it "should show the user with the most orders" do
     visit admin_analytics_dashboard_path
     expect(page).to have_content("#{@admin.username} has the most orders with 3")
+  end
+
+  it "should show when the highest priced item for each category and orders for that category" do
+    visit admin_analytics_dashboard_path
+
+    expect(page).to have_content("Highest priced item for #{@category1.name}: #{@item1.name} at $#{@item1.price}")
+    expect(page).to have_content("Highest priced item for #{@category1.name}: #{@item3.name} at $#{@item3.price}")
   end
 end
