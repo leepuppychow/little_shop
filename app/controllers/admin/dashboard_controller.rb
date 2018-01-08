0f87bbd175c77b5e7eaeb053bd032378e9fecd24
leepuppychow <leepuppychow@gmail.com>
2018-01-08 12:22:18 -0700

    Added All to Order filter dropdown

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,13 +1,13 @@
 class Admin::DashboardController < Admin::BaseController
 
   def index
-    if params[:status]
-      @orders = Order.where(status: params[:status])
-    else
+    if params[:status] == "All" || !params[:status]
       @orders = Order.all
+    else
+      @orders = Order.where(status: params[:status])
     end
     @orders_by_status = Order.group_by_status_count
-    @statuses = Order.pluck(:status).uniq
+    @statuses = Order.pluck(:status).uniq.unshift("All")
   end
 
 end
