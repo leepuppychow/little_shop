11d3febc1e110d8654549a2d155a37de6a70a7a5
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-09 16:51:12 -0700

    added gem 'orderly' and cleaned up retired item analytics test

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -3,7 +3,7 @@ class Admin::AnalyticsController < Admin::BaseController
   def index
     @user = current_user
     @categories = Category.all
-    @items = Item.all
+    @items = Item.ordered_by_retired_count
   end
 
 end
