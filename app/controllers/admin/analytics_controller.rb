2be0b5b626b9b9a4c1034944d85e4625271f304d
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-10 16:42:44 -0700

    refactored analytics controller -removed if statement from analytics#index, moved to a private method

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -4,11 +4,18 @@ class Admin::AnalyticsController < Admin::BaseController
     @user = current_user
     @categories = Category.all
     @items = Item.ordered_by_retired_count
-    if params[:sort_by] == "count"
-      @orders = Order.states_with_count_of_orders_sorted_by_count
-    else
-      @orders = Order.states_with_count_of_orders
-    end
+    arrange_orders
   end
+
+  private
+
+    def arrange_orders
+      if params[:sort_by] == "count"
+        @orders = Order.states_with_count_of_orders_sorted_by_count
+      else
+        @orders = Order.states_with_count_of_orders
+      end
+    end
+
 
 end
