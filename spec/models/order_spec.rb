c48733584d318e7efea7b54302b06bf4acc53b90
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-10 16:35:41 -0700

    Refactored Orders#create by moving some logic to the model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,17 +4,11 @@ describe Order, type: :model do
   describe "Validations" do
     it {is_expected.to validate_presence_of(:status)}
     it {is_expected.to validate_presence_of(:total_price)}
-    it {is_expected.to validate_presence_of(:created_at)}
-    it {is_expected.to validate_presence_of(:updated_at)}
   end
 
  describe "Relationships" do
     it "belongs_to user" do
       is_expected.to belong_to(:user)
     end
-
-    # it "has_many items through order_items" do
-    #   is
-    # end
   end
 end
